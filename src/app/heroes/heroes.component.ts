18a95e018aa76a152a6076f2d9f4d654d502719e
ValterGefali <valtergefali@gmail.com>
2023-01-23 15:30:06 -0300

    ImplentsService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HeroService } from '../hero.service';
+//import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  hero:Hero = {id:1, name:'Wolverine'};
+  heroes:Hero[] = [];
+  selectedHero? :Hero;
+
+  constructor(private heroService:HeroService){}
+
+  ngOnInit(): void {
+   this.getHeroes();
+  }
+
+
+  getHeroes():void
+  {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero:Hero):void
+  {
+    this.selectedHero = hero;
+
+  }
 
 
 }
